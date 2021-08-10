8b990168377d2441d9c4705f0a40e4fdbf5adf17
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-08-10 19:21:05 +0530

    Initial commit

---
+++ date.js
@@ -0,0 +1,17 @@
+
+module.exports = getDate;
+
+function getDate(){
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+  return day;
+
+}
